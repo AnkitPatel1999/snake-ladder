811dc8e183e84ce058f038a5f6acd8b831666430
AJTech1968 <40666123+AJTech1968@users.noreply.github.com>
2020-09-30 14:05:57 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'snake-ladder';
+
+  tile:number[] = new Array(100);
+  color;
+  i=1;
+  constructor() {
+    for (let index = 0; index < this.tile.length; index++) {
+      this.tile[index] = this.i++;
+    }
+    this.tile.reverse();
+    
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
 }
